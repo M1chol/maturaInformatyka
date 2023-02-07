1a6f50638c479f5c93dce63addc9c2300525eaea
M1chol <micho.kozlowskji@gmail.com>
2023-02-07 22:33:24 +0100

    zadanie 3 i 4 z ARKUSZ POKAZOWY (4 marca 2022)

--- Ark_Pokaz_03_2022/AP03_3_5.py
+++ Ark_Pokaz_03_2022/AP03_3_5.py
@@ -1,17 +1,17 @@
-# def pot(a,x, M):
-#     if x==0:
-#         return 1
-#     if x%2==1:
-#         return a*pot(a, x-1, M) % M
-#     temp = pot(a, x/2, M) % M
-#     return (temp**2) % M
+def pot(a,x, M):
+    if x==0:
+        return 1
+    if x%2==1:
+        return a*pot(a, x-1, M) % M
+    temp = pot(a, x/2, M) % M
+    return (temp**2) % M
 
-with open('liczby.txt') as dane:
-    ile=0
-    for line in dane:
-        M, a, b = map(int, line.split())
-        for i in range(M-1):
-            if pow(a, i, M) == b:
-                ile+=1
-                break
-    print(ile)
+# with open('liczby.txt') as dane:
+#     ile=0
+#     for line in dane:
+#         M, a, b = map(int, line.split())
+#         for i in range(M-1):
+#             if pow(a, i, M) == b:
+#                 ile+=1
+#                 break
+#     print(ile)
